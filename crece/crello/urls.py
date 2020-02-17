3943b28396eeae2e5c2a2248e43555b1625454e6
Gema <57403792+Gemamogar@users.noreply.github.com>
2020-02-17 15:00:35 +0100

    hh

--- crece/crello/urls.py
+++ crece/crello/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('crearT/', views.crear_tab, name='crear_tab'),
+    path('contenedorTablero/', views.consultar_tab, name='consultar_tab'),
+    path('crearTablero/', views.crear_tab, name='crear_tab'),
+    path('editarTablero/<int:id>', views.editar_tab, name='editar_tab'),
+    path('eliminarTablero/<int:id>', views.eliminar_tab, name='eliminar_tab'),
+    path('tablero/<int:id>', views.consultar_lista, name='consultar_lista'),
+    path('crearLista/', views.crear_lista, name='crear_lista'),
+
 ]
--- crece/crello/views.py
+++ crece/crello/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.forms import ModelForm
-from crello.models import Tablero
+from crello.models import *
 
 def index(request):
     return render(request,'crello/index.html',{})
@@ -10,7 +10,7 @@ class TableroForm(ModelForm):
         model = Tablero
         fields =['nombre']
 
-''' class ListaForm(ModelForm):
+class ListaForm(ModelForm):
     class Meta:
         model = Lista
         fields =['nombre','fk_Tablero']
@@ -18,13 +18,52 @@ class TableroForm(ModelForm):
 class TarjetaForm(ModelForm):
     class Meta:
         model = Tarjeta
-        fields =['nombre','descripcion','fk_Lista'] '''
+        fields =['nombre','descripcion','fk_Lista']
 
 def crear_tab(request):
     templ = 'crello/crear_tablero.html'
     form =  TableroForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('crello: index.html')# tiene que dirigir a otro
+        return redirect('consultar_tab')
     return render(request,templ,{'form':form})
+
+def crear_lista(request):
+    templ = 'crello/crear_Lista.html'
+    form =  ListaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        #return redirect('consultar_')# tiene que dirigir a otro
+    return render(request,templ,{'form':form})
+
+def consultar_tab(request):
+    templ = 'crello/contenedorTablero.html'
+    listado_tablero=Tablero.objects.all()
+    contexto={}
+    contexto['objectlist']=listado_tablero
+    return render(request,templ,contexto)
+
+def consultar_lista(request):
+    templ = 'crello/tablero.html'
+    listado_lista=Lista.objects.all()
+    contexto={}
+    contexto['objectlist']=listado_lista
+    return render(request,templ,contexto)
+
+def editar_tab(request,id):
+    template = 'crello/editar_tablero.html'
+    info_Tablero = get_object_or_404(Tablero, pk=id)
+    form = TableroForm(request.POST or None, instance=info_Tablero)
+    if form.is_valid():
+        form.save()
+        return redirect('consultar_tab')
+    return render(request,template,{'form':form})
+
+def eliminar_tab(request,id):
+    template = 'crello/eliminar_tablero.html'
+    info_Tablero = get_object_or_404(Tablero, pk=id)
+    if request.method == 'POST':
+        info_Tablero.delete()
+        return redirect('consultar_tab')
+    return render(request,template,{'info_Tablero':info_Tablero})
 
